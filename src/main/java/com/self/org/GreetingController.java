2b14cceb7e5b4417609b7c1d03a75d4d7ff22498
Erasyl <era.berikbol@gmail.com>
2021-10-12 22:18:14 +0600

    letsCode. lesson 2 - connecting database

--- src/main/java/com/self/org/GreetingController.java
+++ src/main/java/com/self/org/GreetingController.java
@@ -1,13 +1,20 @@
 package com.self.org;
 
+import com.self.org.domain.Message;
+import com.self.org.repos.MessageRepo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.Map;
 
 @Controller
 public class GreetingController {
+
+    @Autowired
+    private MessageRepo messageRepo;
 
 
     @GetMapping("/greeting")
@@ -18,9 +25,38 @@ public class GreetingController {
     }
 
     @GetMapping
-    public static String home(Map<String, Object> model) {
+    public String main(Map<String, Object> model) {
+        Iterable<Message> messages = messageRepo.findAll();
+        model.put("messages", messages);
+        return "main";
+    }
 
-        model.put("some", "Main page!!!");
-        return "home";
+    @PostMapping
+    public String add(@RequestParam String text, @RequestParam String tag, Map<String, Object> model) {
+        Message message = new Message(text, tag);
+        messageRepo.save(message);
+
+        // Displaying data, bad practice
+        Iterable<Message> messages = messageRepo.findAll();
+        model.put("messages", messages);
+
+        return "main";
+    }
+
+    @PostMapping("filter")
+    public String filter(@RequestParam String filter, Map<String, Object> model) {
+
+        Iterable<Message> messages;
+
+        if (filter != null && !filter.isEmpty()) {
+            messages = messageRepo.findByTag(filter);
+        } else {
+            messages = messageRepo.findAll();
+        }
+
+
+        model.put("messages", messages);
+
+        return "main";
     }
 }
---
+++ src/main/java/com/self/org/repos/MessageRepo.java
@@ -0,0 +1,11 @@
+package com.self.org.repos;
+
+import com.self.org.domain.Message;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+public interface MessageRepo extends CrudRepository<Message, Integer> {
+
+    List<Message> findByTag(String tag);
+}
